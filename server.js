91addf6ed6517e7835ba40b947b7f6ed4bcff31b
Lucas Lemonnier <ll@ttwebco.com>
2016-12-05 21:02:36 -0500

    retrieve array of approved confessions

--- server.js
+++ server.js
@@ -9,19 +9,38 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 var url = 'mongodb://localhost:27017/devconfessions';
 MongoClient.connect(url, function(err, db) {
+	if (err) {
 	console.log(err);
+	}
 	
-	// show confessions
+	// get approved confessions
 	app.get('/confession', (req, res) => {
-		db.collection('confessions').aggregate([{'$sample': {'size': 1 }}], function(err, item) {
+		db.collection('confessions').find({status:''}, {confession: 1, _id: 0}).toArray(function(err, items) {
 			if (err) {
 				return res.status(500).json({
 					error: err.message
 				});
 			}
-			res.json(item);
+			// create array of confessions
+			let confessionsArray = items.map(function(item) {
+				return item.confession;
+			});
+			res.json(confessionsArray);
 		});
 	});
+
+	// Get random approved confession from db using aggregate
+	// app.get('/confession', (req, res) => {
+	// 	db.collection('confessions').aggregate([{'$sample': {'size': 1 }}, {status:''}], function(err, item) {
+	// 		if (err) {
+	// 			return res.status(500).json({
+	// 				error: err.message
+	// 			});
+	// 		}
+	// 		res.json(item);
+	// 	});
+	// });
+	// 
 
 	// add confession
 	app.post('/confession', (req, res) => {
@@ -38,7 +57,7 @@ MongoClient.connect(url, function(err, db) {
 			res.redirect('/');
 		})
 	})
-});
+}); // end MongoClient
 
 app.listen(4000, function() {
   console.log('listening on 4000')
